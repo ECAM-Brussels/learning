0a292ab040189bb8a9d6fe4ab8d3ee94a26dd819
Khoi Nguyen <khoi@nguyen.me.uk>
2026-04-05 14:12:17 +0200

    Minor changes

--- packages/core/src/fields.tsx
+++ packages/core/src/fields.tsx
@@ -21,43 +21,41 @@ export function Math(label: string) {
     label,
     base,
     feedback: v.pipe(v.string(), v.transform(expr)),
-    Component: function (props) {
+    Component: (props) => {
       const valid = createMemo(() => v.safeParse(base, props.state[props.name]).success)
       return (
-        <>
-          <Show
-            when={!props.question}
-            fallback={
-              <span title={props.label}>
-                <Show when={props.value}>
-                  <Latex value={props.value!} />
-                </Show>
-              </span>
-            }
-          >
-            <input
-              class={[
-                'rounded border p-2 outline-none',
-                {
-                  'bg-slate-50': props.readOnly === true,
-                  'border-red-200': !valid(),
-                  'border-green-500': valid(),
-                },
-              ]}
-              placeholder={props.label}
-              title={props.label}
-              value={props.state.value?.rawInput ?? props.value?.rawInput ?? ''}
-              readonly={props.question || props.readOnly}
-              onInput={(event) => {
-                if (!props.question) {
-                  props.setState((s) => {
-                    s[props.name] = event.target.value
-                  })
-                }
-              }}
-            />
-          </Show>
-        </>
+        <Show
+          when={!props.question}
+          fallback={
+            <span title={props.label}>
+              <Show when={props.value}>
+                <Latex value={props.value!} />
+              </Show>
+            </span>
+          }
+        >
+          <input
+            class={[
+              'rounded border p-2 outline-none',
+              {
+                'bg-slate-50': props.readOnly === true,
+                'border-red-200': !valid(),
+                'border-green-500': valid(),
+              },
+            ]}
+            placeholder={props.label}
+            title={props.label}
+            value={props.state.value?.rawInput ?? props.value?.rawInput ?? ''}
+            readonly={props.question || props.readOnly}
+            onInput={(event) => {
+              if (!props.question) {
+                props.setState((s) => {
+                  s[props.name] = event.target.value
+                })
+              }
+            }}
+          />
+        </Show>
       )
     },
   })
