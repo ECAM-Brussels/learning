eeacd6a90e2c3b7d47b9f98833557fb60981e458
Khoi Nguyen <khoi@nguyen.me.uk>
2026-05-08 09:55:25 +0200

    Minor style change

--- packages/components/src/Python.tsx
+++ packages/components/src/Python.tsx
@@ -6,7 +6,7 @@ export default function Python(props: { value: string; math?: boolean }): JSX.El
   const ready = createMemo(pyodideStatus)
   const output = createMemo(() => runPython(props.value, { math: props.math ?? false }))
   return (
-    <pre class="my-8 rounded-xl bg-slate-200 p-4 shadow-sm">
+    <pre class="my-8 rounded-xl p-4 shadow-sm">
       <Loading fallback="Initializing Python...">
         {ready() && ''}
         <Loading fallback="Executing code...">
