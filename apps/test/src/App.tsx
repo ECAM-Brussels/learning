ec152c99ee504c59d612956e8274accb5b7bfaea
Khoi Nguyen <khoi@nguyen.me.uk>
2026-05-16 14:09:48 +0200

    Update example

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -1,68 +1,73 @@
 import Code from '@learning/components/Code'
-import Latex from '@learning/components/Latex'
+import Environment from '@learning/components/Environment'
+import Latex, { tex } from '@learning/components/Latex'
 import Slide from '@learning/components/Slide'
+import Slideshow from '@learning/components/Slideshow'
 import { $, encrypt, ExerciseSequence, expr, Practice } from '@learning/core'
 import Factor from '@learning/exercises/math/factor'
 import Simple from '@learning/exercises/math/simple'
 import Python from '@learning/exercises/python/code'
 import dedent from 'dedent'
-import { sample } from 'es-toolkit'
-import { Loading } from 'solid-js'
+import { sample, sampleSize } from 'es-toolkit'
+import { createMemo, Loading } from 'solid-js'
 import './style.css'
 
 export default function App() {
   return (
     <Loading>
-      <Slide title="Calcul numérique">
-        <p>
-          L'objectif de cette session est la découverte du langage de programmation <em>Python</em>.
-        </p>
-        <p>
-          Cette page est <strong>interactive</strong>. N'hésitez pas à modifier les codes
-          ci-dessous, et le résultat changera.
-        </p>
-      </Slide>
-      <Slide title="Opérations arithmétiques">
-        <Code lang="python" run math>
-          {dedent /* python */ `
+      {(() => {
+        const test = createMemo(() => {
+          const [x1, x2] = sampleSize([1, 2, 3, 4, 5, 6], 2)
+          return expr(`(x - ${x1})(x - ${x2})`).expand().latex()
+        })
+        return <Factor expr={test()} />
+      })()}
+      <Slideshow>
+        <Slide title="Calcul numérique">
+          <p>
+            L'objectif de cette session est la découverte du langage de programmation{' '}
+            <em>Python</em>.{tex`x^2`}
+          </p>
+          {tex.block`\int_0^1 x^2 \, \mathrm{d} x = \frac 1 3`}
+          <p>
+            Cette page est <strong>interactive</strong>. N'hésitez pas à modifier les codes
+            ci-dessous, et le résultat changera.
+          </p>
+        </Slide>
+        <Slide title="Opérations arithmétiques">
+          <Code lang="python" run math>
+            {dedent /* python */ `
             3 * 4
           `}
-        </Code>
-        <div class="rounded-xl border border-slate-500 bg-slate-50 p-2">
+          </Code>
+          <Environment type="example">
+            <p>Selon Python, que vaut {tex`0.1 \times 0.1`}</p>
+            <div class="flex items-center justify-end gap-4">
+              Votre réponse:
+              <Simple id="test" answer={$(() => encrypt(`${0.1 * 0.1}`))} />
+            </div>
+          </Environment>
+        </Slide>
+        <Slide title="Fonctions">
           <p>
-            Selon Python, que vaut <Latex value="0.1 \times 0.1" />?
+            Définis une fonction <Latex value="f(x) = x^2" /> en Python
           </p>
-          <div class="flex items-center justify-end gap-4">
-            Votre réponse:
-            <Simple answer={$(() => encrypt(`${0.1 * 0.1}`))} />
-          </div>
-        </div>
-      </Slide>
-      <Slide title="Fonctions">
-        <p>
-          Définis une fonction <Latex value="f(x) = x^2" /> en Python
-        </p>
-        <Python
-          id="x-squared"
-          tests={[
-            { test: 'f(0)', result: '0' },
-            { test: 'f(1)', result: '1' },
-            { test: 'f(2)', result: '4' },
-            { test: 'f(-1)', result: '1' },
-          ]}
-        />
-      </Slide>
+          <Python
+            id="x-squared"
+            tests={[0, 1, 2, -1, -26, 75].map((x) => ({ test: `f(${x})`, result: `${x ** 2}` }))}
+          />
+        </Slide>
+      </Slideshow>
       <main class="container mx-auto">
         <ExerciseSequence>
           <Factor expr={$(() => expr('(x + 2) (x + 1)').expand().latex())} />
           <Factor expr="x^2 - 1" />
           <div class="prose">
             <h1>Bonjour</h1>
-            <p>
-              Que vaut <Latex value="\int_0^1 1 \, \mathrm{d} x" />?
-            </p>
+            <p>Que vaut {tex`\int_0^1 x^2 \, \mathrm{d} x`}?</p>
             <div class="flex items-center justify-center gap-4">
               <Latex value="\int_0^1 1 \, \mathrm{d} x =" displayMode />
+              {tex.block`\int_0^1 x^2 \, \mathrm{d} x =`}
               <Simple answer={$(() => encrypt('1'))} />
             </div>
           </div>
