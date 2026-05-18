e25d6d3d47f3b9a8720256655f8c66ebfa0efd5c
Khoi Nguyen <khoi@nguyen.me.uk>
2026-05-18 07:34:09 +0200

    Use pagination

--- packages/core/src/exercise/sequence.tsx
+++ packages/core/src/exercise/sequence.tsx
@@ -1,4 +1,6 @@
-import { Repeat, type JSX } from 'solid-js'
+import Pagination from '@learning/components/Pagination'
+import { range } from 'es-toolkit'
+import { createMemo, refresh, type JSX } from 'solid-js'
 import { ExerciseContext } from './base'
 
 type Props = {
@@ -9,19 +11,35 @@ type Props = {
 export function ExerciseSequence(props: Props) {
   const key = (i: number) =>
     `sequence:${window.location.pathname}:${window.location.search}:${props.id ?? ''}:${i}`
+  const progress = createMemo(() => {
+    return range(props.children.length).map((k) => {
+      try {
+        const exercise = JSON.parse(localStorage.getItem(key(k)) ?? 'null')
+        return (
+          exercise.attempt.every((part: any) => part.correct) &&
+          exercise.attempt.at(-1)?.next === null
+        )
+      } catch {
+        return null
+      }
+    })
+  })
   return (
-    <Repeat count={props.children.length}>
-      {(i) => (
+    <Pagination progress={progress()}>
+      {range(props.children.length).map((i) => () => (
         <ExerciseContext
           value={{
             fetch: () => JSON.parse(localStorage.getItem(key(i)) ?? 'null'),
-            save: (_id, exercise) => localStorage.setItem(key(i), JSON.stringify(exercise)),
+            save: (_id, exercise) => {
+              localStorage.setItem(key(i), JSON.stringify(exercise))
+              refresh(() => progress())
+            },
             reset: () => localStorage.removeItem(key(i)),
           }}
         >
           {props.children[i]}
         </ExerciseContext>
-      )}
-    </Repeat>
+      ))}
+    </Pagination>
   )
 }
