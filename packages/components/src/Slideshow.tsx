8323c9662df03c65b50b0716e43df77263d95f24
Khoi Nguyen <khoi@nguyen.me.uk>
2026-05-20 10:20:24 +0200

    Change size

--- packages/components/src/Slideshow.tsx
+++ packages/components/src/Slideshow.tsx
@@ -17,7 +17,7 @@ import type { JSX } from 'solid-js/jsx-runtime'
  */
 export default function Slideshow(props: { class?: string; children: JSX.Element }) {
   return (
-    <div class={['snap-y snap-mandatory overflow-scroll border', props.class ?? 'h-135 w-240']}>
+    <div class={['snap-y snap-mandatory overflow-scroll border', props.class ?? 'h-270 w-480']}>
       {props.children}
     </div>
   )
