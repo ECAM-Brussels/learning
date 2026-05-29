6301095f31ca84439b1649af12b8160f94b83746
Khoi Nguyen <khoi@nguyen.me.uk>
2026-05-29 13:01:51 +0200

    Reactive page list

--- packages/components/src/Pagination.tsx
+++ packages/components/src/Pagination.tsx
@@ -1,5 +1,5 @@
 import { Dynamic } from '@solidjs/web'
-import { createEffect, createSignal, Repeat, type JSX } from 'solid-js'
+import { createEffect, createSignal, For, type JSX } from 'solid-js'
 
 export function Pagination(props: {
   current?: number
@@ -22,24 +22,24 @@ export function Pagination(props: {
         >
           ‹
         </button>
-        <Repeat count={props.children.length}>
+        <For each={Array.from(Array(props.children.length).keys())}>
           {(i) => (
             <button
               class={[
                 'cursor-pointer border-gray-50 px-3 py-1 shadow',
                 {
-                  'border border-sky-700 font-bold text-sky-700': current() === i + 1,
-                  'text-gray-400': current() !== i + 1,
-                  'bg-green-100': props.progress?.[i] === true,
-                  'bg-red-100': props.progress?.[i] === false,
+                  'border border-sky-700 font-bold text-sky-700': current() === i() + 1,
+                  'text-gray-400': current() !== i() + 1,
+                  'bg-green-100': props.progress?.[i()] === true,
+                  'bg-red-100': props.progress?.[i()] === false,
                 },
               ]}
-              onClick={() => setCurrent(i + 1)}
+              onClick={() => setCurrent(i() + 1)}
             >
-              {i + 1}
+              {i() + 1}
             </button>
           )}
-        </Repeat>
+        </For>
         <button
           class="cursor-pointer px-3 py-1 text-gray-400 shadow"
           onClick={() => setCurrent((prev) => Math.min(prev + 1, props.children.length))}
