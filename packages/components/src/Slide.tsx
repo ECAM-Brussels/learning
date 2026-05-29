4c9a08c511fded9d37bcfd882d569928c7f70539
Khoi Nguyen <khoi@nguyen.me.uk>
2026-05-29 23:24:42 +0200

    Improve slide

--- packages/components/src/Slide.tsx
+++ packages/components/src/Slide.tsx
@@ -2,7 +2,7 @@ import { Loading, Show } from 'solid-js'
 import type { JSX } from 'solid-js/jsx-runtime'
 
 type Props = {
-  class?: JSX.ClassList | string
+  class?: string
   children: JSX.Element
   title?: JSX.Element
 }
@@ -19,12 +19,12 @@ type Props = {
  */
 export default function Slide(props: Props) {
   return (
-    <div class="h-full w-full snap-start">
+    <div class="h-full w-full snap-start overflow-hidden">
       <Show when={props.title}>
         <h3 class="m-0 bg-slate-800 p-4 text-2xl font-medium text-slate-50">{props.title}</h3>
       </Show>
-      <div class="flex h-full w-full">
-        <div class="prose prose-p:leading-loose w-full self-center p-4 text-2xl">
+      <div class="prose prose-li:leading-normal prose-p:leading-loose flex h-full w-full max-w-none text-2xl">
+        <div class={['self-center', props.class]}>
           <Loading fallback="Chargement du slide...">{props.children}</Loading>
         </div>
       </div>
