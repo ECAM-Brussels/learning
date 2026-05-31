9410e6606de58166f0b4be4b635832f9f51b7022
Khoi Nguyen <khoi@nguyen.me.uk>
2026-05-31 13:52:49 +0200

    Add support for syntax highlighting

---
+++ packages/components/src/Highlight.tsx
@@ -0,0 +1,25 @@
+import dedent from 'dedent'
+import { codeToHtml } from 'shiki'
+import { createMemo, merge, omit } from 'solid-js'
+
+type Options = Parameters<typeof codeToHtml>[1]
+
+export function Highlight(props: { code: string; class?: string } & Partial<Options>) {
+  const options = merge({ lang: 'js', theme: 'github-light' }, omit(props, 'code', 'class'))
+  const html = createMemo(() => codeToHtml(props.code, options))
+  return (
+    <pre
+      class={['not-prose mx-4 rounded-md border border-gray-100 p-4 shadow-sm', props.class]}
+      innerHTML={html()}
+    />
+  )
+}
+
+export function hl(lang: string) {
+  return (strings: TemplateStringsArray, ...values: unknown[]) => {
+    const code = createMemo(() =>
+      dedent(strings.reduce((acc, str, i) => acc + str + (values[i] ?? ''), '')),
+    )
+    return <Highlight code={code()} lang={lang} theme="github-light" />
+  }
+}
