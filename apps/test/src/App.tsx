3ac0d2e5d6e64c002199163dcfdb19f848d91b4f
Khoi Nguyen <khoi@nguyen.me.uk>
2026-07-10 11:33:34 +0200

    Add example

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -1,5 +1,5 @@
 import { Attempt, Code, Page } from '@learning/components'
-import { expr, Step } from '@learning/core'
+import { Exercise, expr, Step, tex } from '@learning/core'
 import { Factor } from '@learning/exercises/math/algebra/Factor'
 import { PythonCode } from '@learning/exercises/python/Code'
 import dedent from 'dedent'
@@ -7,6 +7,57 @@ import { allKeyed, sample } from 'es-toolkit'
 
 export default () => (
   <Page title="Tests">
+    <Exercise
+      id="automatic-differentiation"
+      schema={{ data: { expr: 'expr' }, inputs: { attempt: 'expr' } }}
+      data={{ expr: tex.raw`(x + \epsilon)^2` }}
+      correct={(ctx) => ctx.inputs.attempt.isEqual(ctx.data.expr)}
+      prompt={(ctx) => (
+        <>
+          <p>Développez l'expression {tex`${ctx.data.expr}`}</p>
+          <Attempt>
+            {tex`${ctx.data.expr} =`}
+            {ctx.inputs.attempt}
+          </Attempt>
+        </>
+      )}
+    >
+      {(ctx) => (
+        <>
+          <p>
+            Correct! Maintenant, imaginons que nous sommes dans un monde où {tex`\epsilon > 0`} mais{' '}
+            {tex`\epsilon^2 = 0`}. Simplifiez votre expression en utilisant cette hypothèse.
+          </p>
+          <Exercise
+            schema={{ data: { expr: 'expr' }, inputs: { attempt: 'expr' } }}
+            data={ctx.data}
+            correct={(ctx) => ctx.inputs.attempt.isEqual(ctx.data.expr.taylor('\\epsilon', 0, 1))}
+            prompt={(ctx) => (
+              <>
+                <Attempt>
+                  {tex`${ctx.data.expr} =`}
+                  {ctx.inputs.attempt}
+                </Attempt>
+              </>
+            )}
+          >
+            <p>
+              L'hypothèse {tex`\epsilon^2 = 0`} nous a permis de calculer l'
+              <strong>approximation linéaire</strong> de {tex`${ctx.data.expr}`} autour de{' '}
+              {tex`\epsilon = 0`}:
+            </p>
+            {tex`
+              \underbrace{${ctx.data.expr}}_{f(x + \epsilon)}
+              = \underbrace{${ctx.data.expr.subs({ epsilon: 0 }).simplify()}}_{f(x)}
+              + \underbrace{${ctx.data.expr.diff().subs({ epsilon: 0 }).simplify()}}_{f'(x)} \epsilon
+            `}
+            <p>
+              On parle de <strong>dérivation automatique</strong>
+            </p>
+          </Exercise>
+        </>
+      )}
+    </Exercise>
     <Code lang="python" run math>
       {dedent /* python */ `
         from sympy import *
@@ -31,8 +82,9 @@ export default () => (
       prompt="Écris un programme qui affiche le carré d'un nombre."
       tests={[1, 2, 3].map((n) => ({
         test: `f(${n})`,
-        check: (output) => output.result?.trim() === `${n ** 2}`,
+        check: (output) => output.result === `${n ** 2}`,
       }))}
+      check={() => true}
     />
     <Step
       id="standalone"
