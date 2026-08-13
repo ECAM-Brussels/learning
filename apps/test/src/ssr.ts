860d2b796ed6986d872cf364cc18d7ee16e4d9e4
Khoi Nguyen <khoi@nguyen.me.uk>
2026-08-13 18:07:32 +0200

    Revert "Test worker request"
    This reverts commit 56963b828c54d6e4204b25a45361589d0b56c211.

--- apps/test/src/ssr.ts
+++ apps/test/src/ssr.ts
@@ -1,18 +1,7 @@
 import { handleRequest } from 'virtual:solid-ssr-handler'
 
 export default {
-  async fetch(request: Request) {
-    console.log('WORKER REQUEST', request.method, new URL(request.url).pathname)
-
-    try {
-      const response = await handleRequest(request)
-
-      console.log('WORKER RESPONSE', response.status)
-
-      return response
-    } catch (error) {
-      console.error('WORKER ERROR', error)
-      throw error
-    }
+  fetch(request: Request) {
+    return handleRequest(request)
   },
 }
