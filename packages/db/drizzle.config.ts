2d4914f5b6d27e075b7b1c601e980b8ac81ed918
Khoi Nguyen <khoi@nguyen.me.uk>
2026-08-17 16:27:35 +0200

    Drizzle config should not have solid specific code

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'drizzle-kit'
-import 'server-only'
-import { env } from 'virtual:env/server'
 
 export default defineConfig({
   out: './drizzle',
   schema: './schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 })
