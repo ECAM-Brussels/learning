43dece24d85414a243c13a3890d354bfc6517878
Khoi Nguyen <khoi@nguyen.me.uk>
2026-08-20 11:12:52 +0200

    Remove unique for soft delete

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,36 +1,23 @@
-import {
-  boolean,
-  integer,
-  jsonb,
-  snakeCase,
-  text,
-  timestamp,
-  unique,
-  uuid,
-} from 'drizzle-orm/pg-core'
+import { boolean, integer, jsonb, snakeCase, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 import { user } from './auth'
 export * from './auth'
 
-export const steps = snakeCase.table(
-  'steps',
-  {
-    id: uuid().defaultRandom().primaryKey(),
-    userEmail: text()
-      .notNull()
-      .references(() => user.email),
-    url: text().notNull(),
-    sequenceId: text().notNull(),
-    sequencePosition: integer().notNull().default(0),
-    position: integer().notNull().default(0),
+export const steps = snakeCase.table('steps', {
+  id: uuid().defaultRandom().primaryKey(),
+  userEmail: text()
+    .notNull()
+    .references(() => user.email),
+  url: text().notNull(),
+  sequenceId: text().notNull(),
+  sequencePosition: integer().notNull().default(0),
+  position: integer().notNull().default(0),
 
-    name: text(),
-    data: jsonb().notNull(),
-    state: jsonb().notNull(),
-    correct: boolean(),
-    feedback: jsonb().default({}),
+  name: text(),
+  data: jsonb().notNull(),
+  state: jsonb().notNull(),
+  correct: boolean(),
+  feedback: jsonb().default({}),
 
-    created: timestamp().defaultNow().notNull(),
-    deleted: boolean().notNull().default(false),
-  },
-  (t) => [unique().on(t.userEmail, t.url, t.sequenceId, t.sequencePosition, t.position)],
-)
+  created: timestamp().defaultNow().notNull(),
+  deleted: boolean().notNull().default(false),
+})
