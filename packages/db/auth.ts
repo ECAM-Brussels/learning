47fbc0a571c88614dc73fbcf87f7a056d9ed5cc4
Khoi Nguyen <khoi@nguyen.me.uk>
2026-08-25 15:43:35 +0200

    Upgrade solid, better-auth and schemas

--- packages/db/auth.ts
+++ packages/db/auth.ts
@@ -1,4 +1,4 @@
-import { boolean, index, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { boolean, index, pgTable, text, timestamp, uniqueIndex } from 'drizzle-orm/pg-core'
 
 export const user = pgTable('user', {
   id: text('id').primaryKey(),
@@ -36,6 +36,7 @@ export const account = pgTable(
   'account',
   {
     id: text('id').primaryKey(),
+    issuer: text('issuer').notNull(),
     accountId: text('account_id').notNull(),
     providerId: text('provider_id').notNull(),
     userId: text('user_id')
@@ -53,7 +54,10 @@ export const account = pgTable(
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
   },
-  (table) => [index('account_userId_idx').on(table.userId)],
+  (table) => [
+    uniqueIndex('account_issuer_accountId_uidx').on(table.issuer, table.accountId),
+    index('account_userId_idx').on(table.userId),
+  ],
 )
 
 export const verification = pgTable(
