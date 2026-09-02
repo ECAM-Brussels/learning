ce04d3046d368a664bac727b1a1c57eceb271af2
Khoi Nguyen <khoi@nguyen.me.uk>
2026-09-02 09:55:09 +0200

    Enable rich arguments for server functions

--- apps/ecam/src/App.tsx
+++ apps/ecam/src/App.tsx
@@ -1,5 +1,8 @@
+import { enableRichArguments } from '@solidjs/web/server-functions/rich-args'
 import { Layout } from './Layout'
 import { Router } from './router'
 import './style.css'
+
+enableRichArguments()
 
 export default () => <Router children={Layout} />
