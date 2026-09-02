1b6d980383e5b38cbf35808eb5345adefb960879
Khoi Nguyen <khoi@nguyen.me.uk>
2026-09-02 10:18:04 +0200

    Revert "Enable rich arguments for server functions"
    This reverts commit ce04d3046d368a664bac727b1a1c57eceb271af2.

--- apps/ecam/src/App.tsx
+++ apps/ecam/src/App.tsx
@@ -1,8 +1,5 @@
-import { enableRichArguments } from '@solidjs/web/server-functions/rich-args'
 import { Layout } from './Layout'
 import { Router } from './router'
 import './style.css'
-
-enableRichArguments()
 
 export default () => <Router children={Layout} />
