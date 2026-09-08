592628a8f4ba01613f2c0de95be0a5f4471bb6ac
Ruben Hillewaere <hil@ecam.be>
2026-09-08 14:45:54 +0200

    Creation of modeling

---
+++ apps/ecam/src/routes/modeling/_layout.tsx
@@ -0,0 +1,23 @@
+import { BreadCrumbs, Crumb, Page } from '@learning/components'
+import { getUser } from '@learning/core'
+import type { JSX } from '@solidjs/web/jsx-runtime'
+import { createMemo, Show } from 'solid-js'
+
+export default function Layout(props: { children: JSX.Element }) {
+  const user = createMemo(() => getUser())
+  return (
+    <Page>
+      <BreadCrumbs />
+      <Show when={user() === null}>
+        <p class="mb-4 rounded-lg border border-red-100 bg-red-50 p-4">
+          Si vous êtes étudiant·e à l'ECAM, nous vous invitons à vous connecter pour pouvoir
+          sauvegarder votre progression. En tant qu'invité·e, vous ne sauvegarderez vos réponses que
+          sur cet appareil.
+        </p>
+      </Show>
+      <Crumb href="/modeling" title="Modélisation mathématique">
+        {props.children}
+      </Crumb>
+    </Page>
+  )
+}
