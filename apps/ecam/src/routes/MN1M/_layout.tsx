bd05cb392a3d9ea8c3cb94170139222ff9a366ab
Khoi Nguyen <khoi@nguyen.me.uk>
2026-09-10 11:51:39 +0200

    Add preload in layout

--- apps/ecam/src/routes/MN1M/_layout.tsx
+++ apps/ecam/src/routes/MN1M/_layout.tsx
@@ -1,8 +1,15 @@
 import { BreadCrumbs, Crumb, Page } from '@learning/components'
 import { getUser } from '@learning/core'
+import { type RouteDefinition } from '@solidjs/router'
 import type { JSX } from '@solidjs/web/jsx-runtime'
 import { createMemo, Show } from 'solid-js'
 import { paths } from '../../router'
+
+export const route = {
+  preload() {
+    getUser()
+  },
+} satisfies RouteDefinition
 
 export default function Layout(props: { children: JSX.Element }) {
   const user = createMemo(() => getUser())
