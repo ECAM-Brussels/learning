92fa668c422c6e1d6eda9a849e0ffdad644ed9ab
Khoi Nguyen <khoi@nguyen.me.uk>
2026-09-21 12:57:26 +0200

    Lint

--- packages/core/src/exercise/context.local.ts
+++ packages/core/src/exercise/context.local.ts
@@ -6,27 +6,24 @@ const getStorageId = (ctx: Omit<StepContext, 'position'>) =>
   `${ctx.url}:${ctx.sequenceId}:${ctx.sequencePosition}`
 
 export default {
-  fetchSequence: query(
-    async (ctx: Omit<StepContext, 'position' | 'sequencePosition'>) => {
-      const prefix = `${ctx.url}:${ctx.sequenceId}:`
-      return Object.keys(localStorage)
-        .filter((key) => key.startsWith(prefix))
-        .flatMap((key) => {
-          const sequencePosition = parseInt(key.slice(prefix.length))
-          const steps = JSON.parse(localStorage.getItem(key) ?? '[]') as StoredStep[]
-          return steps.map((step, position) => [sequencePosition, position, step] as const)
-        })
-        .reduce(
-          (sequence, [sequencePosition, position, step]) => {
-            sequence[sequencePosition] ??= {}
-            sequence[sequencePosition][position] = step
-            return sequence
-          },
-          {} as Record<number, Record<number, StoredStep>>,
-        )
-    },
-    'fetchSequence-local',
-  ),
+  fetchSequence: query(async (ctx: Omit<StepContext, 'position' | 'sequencePosition'>) => {
+    const prefix = `${ctx.url}:${ctx.sequenceId}:`
+    return Object.keys(localStorage)
+      .filter((key) => key.startsWith(prefix))
+      .flatMap((key) => {
+        const sequencePosition = parseInt(key.slice(prefix.length))
+        const steps = JSON.parse(localStorage.getItem(key) ?? '[]') as StoredStep[]
+        return steps.map((step, position) => [sequencePosition, position, step] as const)
+      })
+      .reduce(
+        (sequence, [sequencePosition, position, step]) => {
+          sequence[sequencePosition] ??= {}
+          sequence[sequencePosition][position] = step
+          return sequence
+        },
+        {} as Record<number, Record<number, StoredStep>>,
+      )
+  }, 'fetchSequence-local'),
   getProgress: query(async (ctx: Omit<StepContext, 'position' | 'sequencePosition'>) => {
     const prefix = `${ctx.url}:${ctx.sequenceId}:`
     return Object.fromEntries(
